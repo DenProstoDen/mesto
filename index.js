f7f5e39f72a1a37e3a2581a864244f64443aa637
DenProstoDen <vuda.vvv@mail.ru>
2023-05-30 18:31:33 +0400

    finish

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const onOpen = () => {
     let popupOpen = document.querySelector('.popup');
     popupOpen.classList.add('popup__opened');
-    console.log(popupOpen);
 }
 
 const onClose = () => {
@@ -13,31 +12,16 @@ const onColor = () => {
     let likeColor = document.querySelector('.element__like');
     likeColor.classList.toggle('element__like-active')
 }
-// const offColor = () => {
-//     let unlikeColor = document.querySelector('.element__like');
-//     unlikeColor.classList.remove('element__like-active')
-// }
 
-// Находим форму в DOM
+
 let formElement = document.querySelector('.popup__container');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.element');
-let jobInput = document.querySelector('.popup__info');
+let nameInput = document.querySelector('.popup__name');
+let infoInput = document.querySelector('.popup__info');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    nameInput.value = document.querySelector('.profile__name').textContent;
-    jobInput.value = document.querySelector('.profile__specialization').textContent;
-    // Вставьте новые значения с помощью textContent
-    
+    evt.preventDefault();
+    document.querySelector('.profile__name').textContent = nameInput.value ;
+    document.querySelector('.profile__specialization').textContent = infoInput.value;
+    onClose();
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit); 
