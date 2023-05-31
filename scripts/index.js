f145437f4e82f8a8fb2bc4944621c1e9934824e2
DenProstoDen <vuda.vvv@mail.ru>
2023-05-31 18:52:14 +0400

    js fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,19 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__name');
 let infoInput = document.querySelector('.popup__info');
 let popupOpen = document.querySelector('.popup');
-let likeColor = document.querySelector('.element__like');
+// let likeColor = document.querySelector('.element__like');
 
 const onOpen = () => {
     popupOpen.classList.add('popup__opened');
 }
 
 const onClose = () => {
-    popupOpen.classList.remove('popup__opened')
+    popupOpen.classList.remove('popup__opened');
 }
 
-const onColor = () => {
-    likeColor.classList.toggle('element__like_active')
-}
+// const onColor = () => {
+//     likeColor.classList.toggle('element__like_active');
+// }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -22,4 +22,9 @@ function handleFormSubmit (evt) {
     document.querySelector('.profile__specialization').textContent = infoInput.value;
     onClose();
 }
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
+document.querySelector('.profile__pencil').addEventListener("click", onOpen);
+// likeColor.addEventListener('click', onColor)
+
+let buttons = document.querySelectorAll('.element__like');
+console.log(buttons)
