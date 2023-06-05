83d23c65d48beaafac8a5e2ecbc795fb2cec12d2
DenProstoDen <vuda.vvv@mail.ru>
2023-06-05 19:02:48 +0400

    hot fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_type_name');
 let infoInput = document.querySelector('.popup__input_type_info');
 let popupOpen = document.querySelector('.popup');
-let likeColor = document.querySelector('.element__like');
 
 const onOpen = () => {
     popupOpen.classList.add('popup_opened');
@@ -10,11 +9,6 @@ const onOpen = () => {
 
 const onClose = () => {
     popupOpen.classList.remove('popup_opened');
-}
-
-const onColor = (event) => {
-    const color = event.target.closest ('.element__like')
-    color.classList.toggle('element__like_active');
 }
 
 function handleFormSubmit (evt) {
@@ -25,8 +19,4 @@ function handleFormSubmit (evt) {
 }
 formElement.addEventListener('submit', handleFormSubmit);
 document.querySelector('.profile__pencil').addEventListener("click", onOpen);
-likeColor.addEventListener('click', onColor)
 document.querySelector('.popup__close-button').addEventListener("click", onClose);
-
-// let buttons = document.querySelectorAll('.element__like');
-// console.log(buttons)
