497481c34cd4a395e69596c673a729fbd800b36c
DenProstoDen <vuda.vvv@mail.ru>
2023-06-07 16:42:38 +0400

    fix js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let infoInput = document.querySelector('.popup__input_type_info');
-let popupOpened = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let newName = document.querySelector('.profile__name');
 let newJob = document.querySelector('.profile__specialization');
 let openPopup = document.querySelector('.profile__pencil');
@@ -10,11 +10,13 @@ let closePopup = document.querySelector('.popup__close-button');
 
 
 const onOpen = () => {
-    popupOpened.classList.add('popup_opened');
+    popup.classList.add('popup_opened');
+    nameInput.value = newName.textContent;
+    infoInput.value = newJob.textContent;
 }
 
 const onClose = () => {
-    popupOpened.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
