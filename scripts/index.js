e74b56ed2b5e751fce2f862792fecb52103682ba
DenProstoDen <vuda.vvv@mail.ru>
2023-06-22 12:25:18 +0400

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 const root = document.querySelector('.root')
 //popup id section
 const popupContainer = document.querySelector('.popup__container');
-const formElementProfile = popupContainer.querySelector('.popup__form');
+const popupProfileNode = document.querySelector('.popup-add');
 const formElementPlace = document.querySelector('.popup__form_place')
 const profileNameInput = document.querySelector('.popup__input_type_name');
 const profileInfoInput = document.querySelector('.popup__input_type_info');
 const newProfileName = document.querySelector('.profile__name');
 const newProfileJob = document.querySelector('.profile__specialization');
 const buttonOpenPopupProfile = document.querySelector('.profile__pencil');
-const popupProfileNode = document.querySelector('.popup-add')
+const formElementProfile = popupProfileNode.querySelector('.popup__form');
 const popupImgNode = document.querySelector('.img-add');
 //popup place section
 const buttonOpenPopupPlace = document.querySelector('.profile__add-button');
